1f7926e052107e790fc6c4153d22af4d7d3016c9
Kanta <tehayan0111@gmail.com>
2019-10-05 15:08:59 +0900

    20191005 より詳しいOG設定
    -twitterの設定を追加

--- resources/views/login/top.blade.php
+++ resources/views/login/top.blade.php
@@ -1,17 +1,7 @@
 @extends('layouts.default')
 
 @section('head')
-<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/websaite#">
-<meta property="og:url" content="https://konkonta.com/apps/MuteReminder/" />
-<meta property="og:type" content="website" />
-<meta property="og:title" content="あの人は今？チェッカー" />
-<meta property="og:description" content="【登録不要】twitterでミュートしたユーザーを一覧表示　ミュート解除やツイートのチラ見ができるWebアプリ" />
-<meta property="og:site_name" content="あの人は今？チェッカー" />
-<meta property="og:image" content="img/slides/muter-slide2.jpg" />
-
-<meta name="twitter:card" content="summary_large_image" />
-<meta name="twitter:site" content="@cha-han5656" />
-
+@include('common_parts.ogp')
 <link rel="stylesheet" href="css/login_index.css">
 <link rel="stylesheet" href="https://use.typekit.net/jdw4aor.css">
 @endsection
