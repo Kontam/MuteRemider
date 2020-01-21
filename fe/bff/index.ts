b8e9e8920e63ddf3f1d8e26b4bde2f6b975d8716
Kanta <tehayan0111@gmail.com>
2020-01-22 00:55:58 +0900

    connect-session-firebase導入 ローカルのserveでテスト
    - session-storeを導入
    - 暫定対応のsetTimeoutを削除

--- fe/bff/index.ts
+++ fe/bff/index.ts
@@ -1,5 +1,5 @@
 require('dotenv').config()
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 const express = require("express");
 const next = require("next");
 const http = require("http");
@@ -7,14 +7,20 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const port :any = parseInt(process.env.PORT as string, 10) || 80;
 const dev = process.env.NODE_ENV !== "production";
-// const nextApp = next({ dev: false, conf: { distDir: 'next' } });
-const nextApp = next({ dev });
+const nextApp = next({ dev: false, conf: { distDir: 'next' } });
+// const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 import BffConst from './const'; //Nextの初期処理以降に記述しなければDotenvが動作しない
 
 const auth = require('./modules/twitterPassport');
 const passport = auth.passport;
 const router = require('./routes/router');
+const FirebaseStore = require('connect-session-firebase')(session);
+const firebase = require('firebase-admin');
+const ref = firebase.initializeApp({
+  credential: firebase.credential.cert('./serviceAccountKey.json'),
+  databaseURL: "https://muter-263c3.firebaseio.com",
+});
 
 export {};
 
@@ -24,9 +30,12 @@ const app = async () => {
   server.use(cookieParser());
   server.use(
     session({
+      store: new FirebaseStore({
+        database: ref.database()
+      }),
       // secret: process.env.SESSION_SECRET,
       secret: "temp secret",
-      resave: false,
+      resave: true,
       saveUninitialized: false,
       // trueが推奨だがhttps通信が必須になるのでproiductionのみとする
       cookie: { secure: process.env.NODE_ENV === 'production' }
@@ -53,15 +62,15 @@ const app = async () => {
   return server;
 }
 
-app().then((server) => {
-  const devServer = http.createServer(server);
-  devServer.listen(port, (err :any) => {
-    if (err) throw err;
-  });
-});
-
-// export const Next = functions.https.onRequest(async (req:any, res:any) => {
-//   const server = await app();
-//   server(req,res);
+// app().then((server) => {
+//   const devServer = http.createServer(server);
+//   devServer.listen(port, (err :any) => {
+//     if (err) throw err;
+//   });
 // });
+
+export const Next = functions.https.onRequest(async (req:any, res:any) => {
+  const server = await app();
+  server(req,res);
+});
 
