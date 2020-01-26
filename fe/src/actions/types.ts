39593e8da8d164fe32c92cdbe8a13fd46bf5b9ad
Kanta <tehayan0111@gmail.com>
2020-01-26 15:52:28 +0900

    フロントエンドリポジトリ分割のため、このリポジトリからコードを削除
    - FE系のコードや定義ファイルを全て消去

--- fe/src/actions/types.ts
+++
@@ -1,6 +0,0 @@
-const ActionTypes = {
-  SET_APPNAME: 'SET_APPNAME',
-  SET_BASEPATH: 'SET_BASEPATH',
-} as const;
-
-export default ActionTypes;
--- fe/src/components/TwApps/MuteReminder/modules/convertBreak.js
+++
@@ -1,7 +0,0 @@
-
-const convertBreak = (str) => {
-  const convertedStr = str.replace(/\r?\n/g, '<br>');
-  return convertedStr;
-};
-
-export default convertBreak;
